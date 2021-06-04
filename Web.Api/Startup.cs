69c1c519c2504de8c82f6c5dcf03949a3204c033
HamedMosavi <hamedmosavi@yahoo.com>
2021-06-04 16:11:00 +0430

    Security: Headers; HSTS

--- Web.Api/Startup.cs
+++ Web.Api/Startup.cs
@@ -4,27 +4,33 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace Web.Api
 {
+
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHsts(options =>
+            {
+                options.MaxAge = TimeSpan.FromDays(100);
+                options.IncludeSubDomains = true;
+                options.Preload = true;
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseHsts();
 
             app.UseRouting();
 
@@ -37,4 +43,5 @@ namespace Web.Api
             });
         }
     }
+
 }
